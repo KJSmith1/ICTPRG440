0bc21e93b46c6503bdc9d11783adbe9b609359e7
KJSmith1 <kiarrah.smith1@studytafensw.edu.au>
2025-08-30 13:24:11 +1000

    TASK 2.3 COMPLETE

--- src_code/main.py
+++ src_code/main.py
@@ -27,3 +27,47 @@ def printAllAttributeRows(inputPath):
     
     return None
 
+def projectCoordinatesOutputShapefile(inputPath, outputPath, EPSG=9473):
+
+    readSpatialDataReturnGeodataframe(inputPath)
+
+    from pandas import DataFrame
+
+    if isinstance(inputGdf, DataFrame):
+        pass
+    else:
+        raise ValueError ("object returned from readSpatialDataReturnGeodataframe is not a geodataframe")
+    
+    if isinstance(outputPath, str):
+        pass
+    else:
+        raise ValueError ("output file path is not a string")
+    
+    if isinstance(EPSG, int):
+        pass
+    else:
+        raise ValueError ("EPSG is not an integer")
+    
+    try:
+        projectedGdf = inputGdf.to_crs(epsg=EPSG)
+    except Exception as e:
+        print(".......Error.......\n", e)
+    
+    if isinstance(projectedGdf, DataFrame):
+        pass
+    else:
+        raise ValueError ("projected object is not a geodataframe")
+    
+    from ntpath import basename
+    
+    outputFile = outputPath + "\\" + basename(inputPath) + ".shp"
+
+    projectedGdf.to_file(outputFile, driver="ESRI Shapefile")
+
+    return None
+
+
+
+    
+    
+    
