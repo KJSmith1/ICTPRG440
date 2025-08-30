3d72ce02d5d30d6f4512cb9296849a6eb5aa61ff
KJSmith1 <kiarrah.smith1@studytafensw.edu.au>
2025-08-30 14:15:29 +1000

    TASK 3 COMPLETE

--- src_code/main.py
+++ src_code/main.py
@@ -1,14 +1,25 @@
 def readSpatialDataReturnGeodataframe(inputPath):
 
+    """
+    This function reads a spatial data file (e.g., shapefile, kml) 
+    from "inputPath" (a string) and returns
+    "inputGdf" (a geodataframe)
+    """
+    import geopandas as gpd
+
+    #validate that inputPath is a string
     if isinstance(inputPath, str):
         pass
     else:
         raise ValueError ("input file path is not a string")
-    
-    import geopandas as gpd
-    
-    inputGdf = gpd.read_file(inputPath)
+     
+    #validate read file function
+    try:
+        inputGdf = gpd.read_file(inputPath)
+    except Exception as e:
+        print(".......Error.......\n", e)
 
+    #validate that output is a geodataframe
     if isinstance(inputGdf, gpd.GeoDataFrame):
         pass
     else:
@@ -18,8 +29,16 @@ def readSpatialDataReturnGeodataframe(inputPath):
     
 def printAllAttributeRows(inputPath):
 
+    """ 
+    This function reads a spatial data file (e.g., shapefile, kml) 
+    from "inputPath" (a string), converts it to a geodataframe object
+    and prints each row of the attribute table for that ob to the console.
+    Returns None.
+    """
+    #call the function that reads a spatial data file and returns the geodataframe "inputGdf" 
     readSpatialDataReturnGeodataframe(inputPath)
    
+   #print each row of the attribute table in the object returned by readSpatialDataReturnGeodataframe()
     for row in inputGdf:
         print(row)
     
@@ -27,34 +46,49 @@ def printAllAttributeRows(inputPath):
 
 def projectCoordinatesOutputShapefile(inputPath, outputPath, EPSG=9473):
 
+    """
+    This function reads a spatial data file (e.g., shapefile, kml) 
+    from "inputPath" (a string), converts it to a geodataframe object, 
+    projects the coordinates of that object to the specified 
+    "EPSG" (an integer, default = 9473 [GDA2020 Australian Albers])
+    and saves the projected object as a shapefile in 
+    "outputPath" (a string).
+    Returns None.
+    """
+    import geopandas as gpd
+
+    #call the function that reads a spatial data file and returns the geodataframe "inputGdf" 
     readSpatialDataReturnGeodataframe(inputPath)
 
-    import geopandas as gpd
-  
+    #validate that outputPath is a string
     if isinstance(outputPath, str):
         pass
     else:
         raise ValueError ("output file path is not a string")
     
+    #validate that EPSG parameter is an integer
     if isinstance(EPSG, int):
         pass
     else:
         raise ValueError ("EPSG is not an integer")
     
+    #validate function for projection to the specified coordinate system
     try:
         projectedGdf = inputGdf.to_crs(epsg=EPSG)
     except Exception as e:
         print(".......Error.......\n", e)
     
+    #validate the projected object is a geodataframe
     if isinstance(projectedGdf, gpd.GeoDataFrame):
         pass
     else:
         raise ValueError ("projected object is not a geodataframe")
     
+    #retrive original file name from inputPath and define the output file path
     from ntpath import basename
-    
     outputFile = outputPath + "\\" + basename(inputPath) + ".shp"
 
+    #save the projected object as a shapefile in the specified output location
     projectedGdf.to_file(outputFile, driver="ESRI Shapefile")
 
     return None
